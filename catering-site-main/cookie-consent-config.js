d0f28e9717225b98dd647ae9b9c26047033e5775
gilis0559314466-del <gilis0559314466@gmail.com>
2026-05-24 09:53:07 +0300

    Delete wrong uploaded folder

--- catering-site-main/cookie-consent-config.js
+++
@@ -1,7 +0,0 @@
-window.CookieConsentConfig = {
-  companyName: 'גיליז קייטרינג',
-  contactEmail: 'gilis0559314466@gmail.com',
-  contactPhone: '055-931-4466',
-  updatedDate: '2026-05-18',
-  bannerText: 'האתר משתמש בקוקיז לצורכי מדידה ושיפור השירות.'
-};
